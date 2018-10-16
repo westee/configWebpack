ab73f027bd6487b6e0c3d041bc04e5f34b0a7f35
dflk <904885508@qq.com>
2018-10-16 14:17:20 +0800

    addExtactTextPlugin

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const uglify = require('uglifyjs-webpack-plugin');
 var htmlPlugin = require('html-webpack-plugin');
+var extractTextplugin = require('extract-text-webpack-plugin')
+//把CSS从JavasScript代码中分离出来，还有一个是如何处理分离出来后CSS中的图片路径不对问题
 // let base = {
 //   index = 
 // }
@@ -16,7 +18,8 @@ module.exports = {
     filename: '[name].js'
   },
   module: {
-    rules: [{
+    rules: [
+      {
         test: '/\.js$/',
         exclude: /node_modules/,
         use: [{
@@ -25,39 +28,34 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        use: [{
-            loader: 'style-loader'
-          },
-          {
-            loader: 'css-loader'
-          },
-          {
-            loader: 'sass-loader'
-          },
-        ]
+        use: extractTextplugin.extract({
+          fallback: 'style-loader',
+          //如果需要，可以在 sass-loader 之前将 resolve-url-loader 链接进来
+          use: ['css-loader', 'sass-loader']
+        })
       },
       {
-        test:/\.(png|jpg|gif|jpeg)/,  //是匹配图片文件后缀名称
-        use:[{
-            loader:'url-loader', //是指定使用的loader和loader的配置参数
-            options:{
-                limit:500  //是把小于500B的文件打成Base64的格式，写入JS
-            }
+        test: /\.(png|jpg|gif|jpeg)/, //是匹配图片文件后缀名称
+        use: [{
+          loader: 'url-loader', //是指定使用的loader和loader的配置参数
+          options: {
+            limit: 500 //是把小于500B的文件打成Base64的格式，写入JS
+          }
         }]
-    }
+      }
 
     ]
   },
   plugins: [
-    new uglify(),     //压缩打包后的文件
+    new uglify(), //压缩打包后的文件
     new htmlPlugin({
-      minify:{ //是对html文件进行压缩
-          removeAttributeQuotes:true  //removeAttrubuteQuotes是却掉属性的双引号。
+      minify: { //是对html文件进行压缩
+        removeAttributeQuotes: true //removeAttrubuteQuotes是却掉属性的双引号。
       },
-      hash:true, //为了开发中js有缓存效果，所以加入hash，这样可以有效避免缓存JS。
-      template:'./src/index.html' //是要打包的html模版路径和文件名称。
-     
-  })
+      hash: true, //为了开发中js有缓存效果，所以加入hash，这样可以有效避免缓存JS。
+      template: './src/index.html' //是要打包的html模版路径和文件名称。
+    }),
+    new extractTextplugin("css/index.css")
   ],
   devServer: {
     contentBase: path.resolve(__dirname, '../dist'),
